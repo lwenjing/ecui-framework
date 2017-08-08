a2c421eb280cd52ad4946a6462dd6880fac4238d
ouyangxianwei <ouyangxianwei@yxqiche.com>
2017-08-08 17:14:00 +0800

    初始提交

---
+++ build.sh
@@ -0,0 +1,1 @@
+java -jar smarty4j.jar ./ecui.js --left //{ --right }// --charset utf-8 -o ecui-all.js
---
+++ ecui.js
@@ -0,0 +1,67 @@
+//{if 1}//var ecui;//{/if}//
+(function () {
+//{if 0}//
+    document.write('<script type="text/javascript" src="ecui/adapter.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/core.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/control.js"></script>');
+
+    document.write('<script type="text/javascript" src="ecui/items.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/popup.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/label.js"></script>');
+
+    document.write('<script type="text/javascript" src="ecui/button.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/input-control.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/format-input.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/checkbox.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/radio.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/select.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/combox.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/listbox.js"></script>');
+
+    document.write('<script type="text/javascript" src="ecui/dialog.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/tree-view.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/month-view.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/tab.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/progress.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/table.js"></script>');
+
+    document.write('<script type="text/javascript" src="ecui/check-tree.js"></script>');
+
+    document.write('<script type="text/javascript" src="ecui/retina.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/messagebox.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/decorate.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/esr.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/etpl.js"></script>');
+//{/if}//
+
+//{include file="ecui/adapter.js"}//
+//{include file="ecui/tools.js"}//
+//{include file="ecui/core.js"}//
+//{include file="ecui/control.js"}//
+
+//{include file="ecui/items.js"}//
+//{include file="ecui/label.js"}//
+
+//{include file="ecui/button.js"}//
+//{include file="ecui/input-control.js"}//
+//{include file="ecui/format-input.js"}//
+//{include file="ecui/checkbox.js"}//
+//{include file="ecui/radio.js"}//
+//{include file="ecui/select.js"}//
+//{include file="ecui/combox.js"}//
+
+//{include file="ecui/dialog.js"}//
+//{include file="ecui/tree-view.js"}//
+//{include file="ecui/month-view.js"}//
+//{include file="ecui/tab.js"}//
+//{include file="ecui/progress.js"}//
+//{include file="ecui/table.js"}//
+
+//{include file="ecui/check-tree.js"}//
+
+//{include file="ecui/retina.js"}//
+//{include file="ecui/messagebox.js"}//
+//{include file="ecui/decorate.js"}//
+//{include file="ecui/esr.js"}//
+//{include file="ecui/etpl.js"}//
+}());
