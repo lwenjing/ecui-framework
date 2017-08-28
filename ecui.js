5b14ae9f9db2cb1e9e00e012992e9847602b1f78
ouyangxianwei <ouyangxianwei@yxqiche.com>
2017-08-28 16:48:15 +0800

    弹整加载顺序

--- ecui.js
+++ ecui.js
@@ -7,9 +7,17 @@
 
     document.write('<script type="text/javascript" src="ecui/items.js"></script>');
     document.write('<script type="text/javascript" src="ecui/popup.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/label.js"></script>');
 
     document.write('<script type="text/javascript" src="ecui/button.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/dialog.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/label.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/month-view.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/progress.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/retina.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/tab.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/table.js"></script>');
+    document.write('<script type="text/javascript" src="ecui/tree-view.js"></script>');
+
     document.write('<script type="text/javascript" src="ecui/text.js"></script>');
     document.write('<script type="text/javascript" src="ecui/checkbox.js"></script>');
     document.write('<script type="text/javascript" src="ecui/radio.js"></script>');
@@ -17,18 +25,10 @@
     document.write('<script type="text/javascript" src="ecui/combox.js"></script>');
     document.write('<script type="text/javascript" src="ecui/listbox.js"></script>');
 
-    document.write('<script type="text/javascript" src="ecui/dialog.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/tree-view.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/month-view.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/tab.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/progress.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/table.js"></script>');
-
     document.write('<script type="text/javascript" src="ecui/calendar.js"></script>');
     document.write('<script type="text/javascript" src="ecui/calendar-input.js"></script>');
     document.write('<script type="text/javascript" src="ecui/check-tree.js"></script>');
 
-    document.write('<script type="text/javascript" src="ecui/retina.js"></script>');
     document.write('<script type="text/javascript" src="ecui/messagebox.js"></script>');
 
     document.write('<script type="text/javascript" src="ecui/decorate.js"></script>');
