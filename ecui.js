31d844e41be1d322deaba6ef88faea64ca7d5bc8
ouyangxianwei <ouyangxianwei@yxqiche.com>
2017-08-30 09:43:15 +0800

    修改目录

--- ecui.js
+++ ecui.js
@@ -1,41 +1,41 @@
 //{if 1}//var ecui;//{/if}//
 (function () {
-    document.write('<script type="text/javascript" src="ecui/adapter.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/core.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/control.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/input-control.js"></script>');
+    document.write('<script type="text/javascript" src="src/adapter.js"></script>');
+    document.write('<script type="text/javascript" src="src/core.js"></script>');
+    document.write('<script type="text/javascript" src="src/control.js"></script>');
+    document.write('<script type="text/javascript" src="src/input-control.js"></script>');
 
-    document.write('<script type="text/javascript" src="ecui/items.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/popup.js"></script>');
+    document.write('<script type="text/javascript" src="src/items.js"></script>');
+    document.write('<script type="text/javascript" src="src/popup.js"></script>');
 
-    document.write('<script type="text/javascript" src="ecui/button.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/dialog.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/label.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/month-view.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/progress.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/retina.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/tab.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/table.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/tree-view.js"></script>');
+    document.write('<script type="text/javascript" src="src/button.js"></script>');
+    document.write('<script type="text/javascript" src="src/dialog.js"></script>');
+    document.write('<script type="text/javascript" src="src/label.js"></script>');
+    document.write('<script type="text/javascript" src="src/month-view.js"></script>');
+    document.write('<script type="text/javascript" src="src/progress.js"></script>');
+    document.write('<script type="text/javascript" src="src/retina.js"></script>');
+    document.write('<script type="text/javascript" src="src/tab.js"></script>');
+    document.write('<script type="text/javascript" src="src/table.js"></script>');
+    document.write('<script type="text/javascript" src="src/tree-view.js"></script>');
 
-    document.write('<script type="text/javascript" src="ecui/text.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/checkbox.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/radio.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/select.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/combox.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/listbox.js"></script>');
+    document.write('<script type="text/javascript" src="src/text.js"></script>');
+    document.write('<script type="text/javascript" src="src/checkbox.js"></script>');
+    document.write('<script type="text/javascript" src="src/radio.js"></script>');
+    document.write('<script type="text/javascript" src="src/select.js"></script>');
+    document.write('<script type="text/javascript" src="src/combox.js"></script>');
+    document.write('<script type="text/javascript" src="src/listbox.js"></script>');
 
-    document.write('<script type="text/javascript" src="ecui/calendar.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/calendar-input.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/check-tree.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/multilevel-select.js"></script>');
+    document.write('<script type="text/javascript" src="src/calendar.js"></script>');
+    document.write('<script type="text/javascript" src="src/calendar-input.js"></script>');
+    document.write('<script type="text/javascript" src="src/check-tree.js"></script>');
+    document.write('<script type="text/javascript" src="src/multilevel-select.js"></script>');
 
-    document.write('<script type="text/javascript" src="ecui/messagebox.js"></script>');
+    document.write('<script type="text/javascript" src="src/messagebox.js"></script>');
 
-    document.write('<script type="text/javascript" src="ecui/decorate.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/effect.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/etpl.js"></script>');
-    document.write('<script type="text/javascript" src="ecui/esr.js"></script>');
+    document.write('<script type="text/javascript" src="src/decorate.js"></script>');
+    document.write('<script type="text/javascript" src="src/effect.js"></script>');
+    document.write('<script type="text/javascript" src="src/etpl.js"></script>');
+    document.write('<script type="text/javascript" src="src/esr.js"></script>');
 //{if 0}//
     document.write('<script type="text/javascript" src="debug.js"></script>');
 //{/if}//
