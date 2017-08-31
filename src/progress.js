31e1991d51b64fe3c332873c8169a8ec2f2b5188
ouyangxianwei <ouyangxianwei@yxqiche.com>
2017-08-31 11:20:36 +0800

    支持进度圆

--- src/progress.js
+++ src/progress.js
@@ -1,37 +1,23 @@
 /*
-Progress - 定义进度显示的基本操作。
-进度条控件，继承自基础控件，面向用户显示一个任务执行的程度。
+Progress - 定义进度显示的基本操作，不建议直接初始化。
+进度控件，继承自基础控件，面向用户显示一个任务执行的程度。
 
-进度条控件直接HTML初始化的例子:
+进度控件直接HTML初始化的例子:
 <div ui="type:progress;max:100;value:0"></div>
 
 属性
-_eText   - 内容区域
-_eMask   - 完成的进度比例内容区域
-_sFormat - 文本显示的格式
 _nValue  - 进度值
 _nMax    - 进度最大值
 */
-(function () {
 //{if 0}//
+(function () {
     var core = ecui,
-        ui = core.ui;
+        ui = core.ui,
+        util = core.util;
 //{/if}//
-    function flush() {
-        var rate = this._nValue / this._nMax;
-        if (this._sFormat) {
-            var text = this._sFormat.replace(/[*$#]/g, function (ch) {
-                    return ch === '*' ? this._nMax : ch === '$' ? this._nValue : Math.round(rate * 100);
-                }.bind(this));
-            this._eText.innerHTML = this._eMask.innerHTML = text;
-        }
-        this._eMask.style.clip = 'rect(0px,' + (rate * this.getWidth()) + 'px,' + this.getHeight() + 'px,0px)';
-    }
-
     /**
-     * 初始化进度条控件。
+     * 初始化进度控件。
      * options 对象支持的属性如下：
-     * format 文本格式
      * max 最大值
      * value 当前值
      * @public
@@ -51,58 +37,72 @@ _nMax    - 进度最大值
             this._sFormat = options.format;
             this._nMax = options.max || 100;
             this._nValue = options.value || 0;
-
-            flush.call(this);
         },
         {
             /**
+             * 进度变化的默认处理。
+             * @protected
+             */
+            $progress: util.blank,
+
+            /**
+             * 获取进度的最大值。
+             * @public
+             *
+             * @return {number} 进度的最大值
+             */
+            getMax: function () {
+                return this._nMax;
+            },
+
+            /**
+             * 获取进度的当前值。
+             * @public
+             *
+             * @return {number} 进度的当前值
+             */
+            getValue: function () {
+                return this._nValue;
+            },
+
+            /**
              * @override
              */
-            $dispose: function () {
-                this._eText = this._eMask = null;
-                ui.Control.prototype.$dispose.call(this);
+            init: function (options) {
+                ui.Control.prototype.init.call(this, options);
+                core.triggerEvent(this, 'progress');
             },
 
             /**
-             * 设置进度条的最大值。
+             * 设置进度的最大值。
              * @public
              *
-             * @param {number} max 进度条的最大值
-             */
-            $initStructure: function (width, height) {
-                ui.Control.prototype.$initStructure.call(this, width, height);
-                this._eText.style.width = this._eMask.style.width = width + 'px';
-                this._eText.style.height = this._eMask.style.height = height + 'px';
-            },
-
-            /**
-             * 设置进度条的最大值。
-             * @public
-             *
-             * @param {number} max 进度条的最大值
+             * @param {number} max 进度的最大值
              */
             setMax: function (max) {
                 max = Math.max(1, max);
                 if (this._nMax !== max) {
                     this._nMax = max;
                     this._nValue = Math.min(this._nValue, this._nMax);
-                    flush.call(this);
+                    core.triggerEvent(this, 'progress');
                 }
             },
 
             /**
-             * 设置进度条的数值。
+             * 设置进度的当前值。
              * @public
              *
-             * @param {number} value 进度条的数值
+             * @param {number} value 进度的当前值
              */
             setValue: function (value) {
                 value = Math.max(Math.min(this._nMax, value), 0);
                 if (this._nValue !== value) {
                     this._nValue = value;
-                    flush.call(this);
+                    core.triggerEvent(this, 'progress');
                 }
             }
         }
     );
+//{if 0}//
 }());
+//{/if}//
