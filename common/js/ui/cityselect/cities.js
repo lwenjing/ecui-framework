b39b858c91bbe33377816cfdc2668a6748b87b57
xuxinlong <xuxinlong@yxqiche.com>
2017-09-15 21:02:14 +0800

    地区控件添加三级联动

--- common/js/ui/cityselect/cities.js
+++ common/js/ui/cityselect/cities.js
@@ -13,45 +13,77 @@ cities - 地区联动下拉框控件。
     var core = ecui,
         ui = core.ui;
 
-
-    var code,
-        key,
-        citys,
-        item,
-        PROVINCE = {},
-        CITY = {};
-    for (code in daikuan.cities) {
-        if (code.slice(2) == '0000') {
-            PROVINCE[code] = daikuan.cities[code];
-        } else if (code.slice(4) == '00') {
-            if (!CITY[code.slice(0, 2) + '0000']) {
-                CITY[code.slice(0, 2) + '0000'] = {};
+    function getCITYS(type) {
+        var code,
+            key,
+            key2,
+            citys,
+            area,
+            item,
+            item2,
+            PROVINCE = {},
+            CITY = {},
+            AREA = {};
+        for (code in daikuan.cities) {
+            if (code.slice(2) == '0000') {
+                PROVINCE[code] = daikuan.cities[code];
+            } else if (code.slice(4) == '00') {
+                if (!CITY[code.slice(0, 2) + '0000']) {
+                    CITY[code.slice(0, 2) + '0000'] = {};
+                }
+                CITY[code.slice(0, 2) + '0000'][code] = daikuan.cities[code];
+            } else {
+                if (!AREA[code.slice(0, 4) + '00']) {
+                    AREA[code.slice(0, 4) + '00'] = {};
+                }
+                AREA[code.slice(0, 4) + '00'][code] = daikuan.cities[code];
             }
-            CITY[code.slice(0, 2) + '0000'][code] = daikuan.cities[code];
         }
+        var CITYS = [];
+        for (code in  PROVINCE) {
+            citys = {
+                value: code,
+                code: PROVINCE[code],
+                children: []
+            };
+            item = CITY[code];
+            for (key in item) {
+                if (type === 'AREA') {
+                    area = {
+                        value: key,
+                        code: item[key],
+                        children: []
+                    };
+                    item2 = AREA[key];
+                    for (key2 in item2) {
+                        area.children.push({
+                            value: key2,
+                            code: item2[key2]
+                        });
+                    }
+                    if (area.children.length <= 0) {
+                        delete area.children;
+                    }
+                    citys.children.push(area);
+                } else if (type === 'CITY') {
+                    citys.children.push({
+                        value: key,
+                        code: item[key]
+                    });
+                }
+            }
+            if (citys.children.length <= 0) {
+                delete citys.children;
+            }
+            CITYS.push(citys);
+        }
+        return CITYS;
     }
-    var CITYS = [];
-    for (code in  PROVINCE) {
-        citys = {
-            value: code,
-            code: PROVINCE[code],
-            children: []
-        };
-        item = CITY[code];
-        for (key in item) {
-            citys.children.push({
-                value: key,
-                code: item[key]
-            });
-        }
-        if (citys.children.length  <= 0) {
-            delete citys.children;
-        }
-        CITYS.push(citys);
-    }
-
     function provinceChange() {
         this.getParent().getSelect(1).setValue(this.getValue());
+    }
+    function cityChange() {
+        this.getParent().getSelect(2).setValue(this.getValue());
     }
     /**
      * 初始化多级联动下拉框控件。
@@ -66,12 +98,17 @@ cities - 地区联动下拉框控件。
             $ready: function (options) {
                 ui.MultilevelSelect.prototype.$ready.call(this, options);
                 var province = this.getSelect(0);
-                this.setData(CITYS);
+                var city = this.getSelect(1);
+                this.setData(getCITYS(this.getSelect(2) ? 'AREA' : 'CITY'));
                 if (options.value && options.value != '' && options.value != '0') {
                     province.setValue(options.value.slice(0, 2) + '0000');
                     core.triggerEvent(province, 'change');
                     core.addEventListener(province, 'change', provinceChange);
                     this.getSelect(1).setValue(options.value.slice(0, 4) + '00');
+                    if (this.getSelect(2)) {
+                        core.triggerEvent(city, 'change');
+                        core.addEventListener(city, 'change', cityChange);
+                    }
                 }
             },
             setValue: function (val) {
