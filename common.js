49d6cac73a941544f03c1670e584459440afed3e
xuxinlong <xuxinlong@yixincapital.com>
2017-10-12 13:14:00 +0800

    去掉common.js中的中文注释

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ var daikuan = {};
 document.write('<script type="text/javascript" src="common/js/utility.js"></script>');
 document.write('<script type="text/javascript" src="common/js/base64.js"></script>');
 document.write('<script type="text/javascript" src="common/js/cityData.js"></script>');
-//控件
+
 document.write('<script type="text/javascript" src="common/js/ui/pagination/pagination.js"></script>');
 document.write('<script type="text/javascript" src="common/js/ui/navTree/navTree.js"></script>');
 document.write('<script type="text/javascript" src="common/js/ui/cityselect/cities.js"></script>');
