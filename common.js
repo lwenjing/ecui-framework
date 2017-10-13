bb12aa5061071e5cfdd437c179e17249945d3457
xuxinlong <xuxinlong@yixincapital.com>
2017-10-13 23:04:22 +0800

    添加drag控件

--- common.js
+++ common.js
@@ -6,3 +6,4 @@ document.write('<script type="text/javascript" src="common/js/cityData.js"></scr
 document.write('<script type="text/javascript" src="common/js/ui/pagination/pagination.js"></script>');
 document.write('<script type="text/javascript" src="common/js/ui/navTree/navTree.js"></script>');
 document.write('<script type="text/javascript" src="common/js/ui/cityselect/cities.js"></script>');
+document.write('<script type="text/javascript" src="common/js/ui/drag/drag.js"></script>');
