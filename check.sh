9e92088a7736ba52df43262e28ed99ad4242817b
ouyangxianwei <ouyangxianwei@yxqiche.com>
2018-02-05 10:26:42 +0800

    修改update.html不存在时的bug

--- check.sh
+++ check.sh
@@ -8,7 +8,13 @@ fi
 
 while :
 do
-	let secs=(`date +"%s"`-`stat -f "%m" update.html`)+1;
+	if [ -f update.html ]
+	then
+		let secs=(`date +"%s"`-`stat -f "%m" update.html`)+1;
+	else
+		let secs=(`date +"%s"`)+1;
+	fi
+
 	if [ `find . -type f -name "*" -mtime -${secs}s | wc -l` -gt 1 ]
 	then
 		echo -e "/*<script>window.onload=function(){value=document.getElementsByTagName('TEXTAREA')[0].value;parent.postMessage({url:location.href.slice(7),text:value.slice(value.indexOf('\\\\n')+1).replace(/&lt;/g,'<').replace(/&gt;/g,'>').replace(/&amp;/g,'&')},'*');}</script><textarea>*/\n"`date +"%s"` > update.html
