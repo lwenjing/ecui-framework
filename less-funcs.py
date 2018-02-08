9f12a38d664734fe99d0c4a538b5719d89db4d5e
ouyangxianwei <ouyangxianwei@yxqiche.com>
2018-02-08 10:22:44 +0800

    代码结构更紧凑

--- less-funcs.py
+++ less-funcs.py
@@ -1,5 +1,18 @@
 import sys,re,json
 
+def getOption(name, defValue):
+        if name in options:
+                return options[name]
+        else:
+                return 75
+
+def f2s(value):
+        value = str(value)
+        if value[-2:] == ".0":
+                return value[:-2]
+        else:
+                return value
+
 def repl(match):
         if hasattr(funcs, match.group(1)):
                 return getattr(funcs, match.group(1))(match.group(2))
@@ -9,11 +22,7 @@ def repl(match):
 class Funcs(object):
         def px2rem(self, value):
                 value = value[:-2]
-                if ('px2rem' in options):
-                        div = options['px2rem']
-                else:
-                        div = 75
-                return str(float(value) / div) + 'rem'
+                return f2s(float(value) / getOption('px2rem', 75)) + 'rem'
 
 funcs = Funcs()
 pFunc = re.compile(r'(\w+)\(([^)]+)\)')
