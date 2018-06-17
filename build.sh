5249a05097f9ae4e70d7d2e1729b08326a8bd254
ouyangxianwei <ouyangxianwei@yxqiche.com>
2018-06-17 12:25:11 +0800

    允许lib-fe自身打包

--- build.sh
+++ build.sh
@@ -1,64 +1,76 @@
-if [ ! $1 ]
+if [ ! $1 ] && [ ! -f smarty4j.jar ]
 then
     echo "build.sh [ProjectName]"
     exit -1
 fi
 
-css_proc='lessc - --plugin=less-plugin-clean-css | python ${path}less-funcs.py "$2"'
-tpl_proc='java -jar ${path}smarty4j.jar --left //{ --right }// --charset utf-8'
-compress_proc='java -jar ${path}webpacker.jar --mode 1 --charset utf-8'
+css_proc='lessc - --plugin=less-plugin-clean-css | python $libpath/less-funcs.py "$2"'
+tpl_proc='java -jar $libpath/smarty4j.jar --left //{ --right }// --charset utf-8'
+compress_proc='java -jar $libpath/webpacker.jar --mode 1 --charset utf-8'
 reg_script="-e \"s/document.write('<script type=\\\"text\/javascript\\\" src=\([^>]*\)><\/script>');/\/\/{include file=\1}\/\//g\""
 reg_comment="-e \"s/[[:space:]]/ /g\" -e \"s/^ *//g\" -e \"s/ *$//g\" -e \"/^ *$/d\" -e \"/<\!-- *$/{N;s/\\n//;}\" -e \"s/<\!-- *-->//g\" -e \"/^ *$/d\" -e \"/<script>window.onload=/d\""
 
-if [ $1 = 'ecui' ]
+if [ $1 ]
 then
-    if [ ! -d release ]
+    if [ $1 = 'ecui' ]
     then
-        mkdir release
+        if [ ! -d release ]
+        then
+            mkdir release
+        fi
+        echo "build ecui-2.0.0"
+        path=""
+        cat ecui.css | eval $css_proc > release/ecui-2.0.0.css
+        eval "sed $reg_script ecui.js" | eval $tpl_proc | eval $compress_proc > release/ecui-2.0.0.js
+        exit 0
     fi
-    echo "build ecui-2.0.0"
-    path=""
-    cat ecui.css | eval $css_proc > release/ecui-2.0.0.css
-    eval "sed $reg_script ecui.js" | eval $tpl_proc | eval $compress_proc > release/ecui-2.0.0.js
-    exit 0
-fi
 
-if [ -f smarty4j.jar ]
-then
+    if [ -f smarty4j.jar ]
+    then
+        flag=1
+        cd ..
+    fi
+
+    if [ ! -d $1 ]
+    then
+        echo "$1 doesn't exist"
+        exit -2
+    fi
+
+    if [ ! -d "$1/common" ]
+    then
+        ln -s ../lib-fe/common "$1/common"
+    fi
+
+    cd $1
+    output="../output-$1"
+    lib="../lib-fe"
+else
     flag=1
-    cd ..
+    output="../output"
+    lib="."
 fi
-if [ ! -d "$1/common" ]
-then
-    ln -s ../lib-fe/common "$1/common"
-fi
+libpath="$lib"
 
-output="output-$1"
 if [ ! -d $output ]
 then
     mkdir $output
 fi
 
-if [ ! -d $1 ]
-then
-    echo "$1 doesn't exist"
-    exit -2
-fi
-
-for file in `ls $1`
+for file in `ls`
 do
-    if [ -d "$1/$file" ]
+    if [ -d "$file" ]
     then
-    	if [ -f "$1/$file/$file.js" ]
+        cd $file
+        libpath="../$lib"
+    	if [ -f "$file.js" ]
     	then
             echo "process module-$file"
-	        if [ ! -d "$output/$file" ]
+	        if [ ! -d "../$output/$file" ]
 	        then
-	            mkdir "$output/$file"
+	            mkdir "../$output/$file"
 	        fi
-	        cd "$1/$file"
-            path="../../lib-fe/"
-	        sed -e "s/\([^A-Za-z0-9_]*\)ecui.esr.loadRoute('\([^']*\)');/\1\/\/{include file='route.\2.js'}\/\//g" -e "s/\([^A-Za-z0-9_]*\)ecui.esr.loadClass('\([^']*\)');/\1\/\/{include file='class.\2.js'}\/\//g" "$file.js" | eval $tpl_proc | eval $compress_proc > "../../$output/$file/$file.js"
+	        sed -e "s/\([^A-Za-z0-9_]*\)ecui.esr.loadRoute('\([^']*\)');/\1\/\/{include file='route.\2.js'}\/\//g" -e "s/\([^A-Za-z0-9_]*\)ecui.esr.loadClass('\([^']*\)');/\1\/\/{include file='class.\2.js'}\/\//g" "$file.js" | eval $tpl_proc | eval $compress_proc > "../$output/$file/$file.js"
             if [ -f "$file.css" ]
             then
                 value="/*{include file=\"$file.css\"}*/\
@@ -76,87 +88,100 @@ s/.*/.&{/
 G
 s/\([^A-Za-z0-9_]\)ecui.esr.loadRoute('\([^']*\)');/\1\/\/{include file=\"route.\2.css\"}\/\/\\
 }/
-}" | eval $tpl_proc | eval $css_proc > "../../$output/$file/$file.css"
+}" | eval $tpl_proc | eval $css_proc > "../$output/$file/$file.css"
 	        sed -e "s/\/\/.*//g" -e "s/\([^A-Za-z0-9_]\)\(ecui.esr.loadRoute('\([^']*\)');\)/\1\\
 \2\\
-/g" "$file.js" | grep "ecui.esr.loadRoute" | sed -e "s/\([^A-Za-z0-9_]*\)ecui.esr.loadRoute('\([^']*\)');/\1\/\/{include file='route.\2.html'}\/\//g" | eval $tpl_proc | eval "sed $reg_comment" > "../../$output/$file/$file.html"
-	        cd ../..
+/g" "$file.js" | grep "ecui.esr.loadRoute" | sed -e "s/\([^A-Za-z0-9_]*\)ecui.esr.loadRoute('\([^']*\)');/\1\/\/{include file='route.\2.html'}\/\//g" | eval $tpl_proc | eval "sed $reg_comment" > "../$output/$file/$file.html"
 	    else
-	    	if [ ! -f "$1/$file/.buildignore" ]
+	    	if [ ! -f ".buildignore" ]
             then
-                if [ ! -d "$output/$file/" ]
+                if [ ! -d "../$output/$file/" ]
                 then
-                    mkdir "$output/$file/"
+                    mkdir "../$output/$file/"
                 fi
-	    		cp -R "$1/$file/"* "$output/$file/"
+                echo "copy $file/"
+	    		cp -R * "../$output/$file/"
 	    	fi
 	    fi
+        libpath="$lib"
+        cd ..
     else
+        if [ ! $1 ]
+        then
+            if [ "${file##*.}" = "jar" ] || [ "${file##*.}" = "sh" ] || [ "${file##*.}" = "py" ] || [ $file = "README.md" ]
+            then
+                continue
+            fi
+        fi
         echo "process file-$file"
-        path="../lib-fe/"
         if [ "${file##*.}" = "js" ]
         then
             name="${file%.*}"
             if [ "${name##*.}" = "min" ]
             then
-                cp "$1/$file" "$output/"
+                cp "$file" "$output/"
             else
-                cd $1
-                eval "sed $reg_script $file" | eval $tpl_proc | eval $compress_proc > "../$output/$file"
-                cd ..
+                eval "sed $reg_script $file" | eval $tpl_proc | eval $compress_proc > "$output/$file"
             fi
         else
             if [ "${file##*.}" = "css" ]
             then
-                cd $1
-                cat "$file" | eval $css_proc > "../$output/$file"
-                cd ..
+                cat "$file" | eval $css_proc > "$output/$file"
             else
                 if [ "${file##*.}" = "html" ]
                 then
-                    java -jar lib-fe/smarty4j.jar "$1/$file" --left \<\!--{ --right }--\> --charset utf-8 | eval "sed -e \"s/stylesheet\/less[^\\\"]*/stylesheet/g\" -e \"s/<header/<div style=\\\"display:none\\\"/g\" -e \"s/<\/header/<\/div/g\" -e \"s/<container/<div ui=\\\"type:layer\\\" style=\\\"display:none\\\"/g\" -e \"s/<\/container/<\/div/g\" $reg_comment" > "$output/$file"
+                    cat "$file" | java -jar "$libpath/smarty4j.jar" --left \<\!--{ --right }--\> --charset utf-8 | eval "sed -e \"s/stylesheet\/less[^\\\"]*/stylesheet/g\" -e \"s/<header/<div style=\\\"display:none\\\"/g\" -e \"s/<\/header/<\/div/g\" -e \"s/<container/<div ui=\\\"type:layer\\\" style=\\\"display:none\\\"/g\" -e \"s/<\/container/<\/div/g\" $reg_comment" > "$output/$file"
                 else
-                    cp "$1/$file" "$output/"
+                    cp "$file" "$output/"
                 fi
             fi
         fi
     fi
 done
 
-cd lib-fe
-for file in `ls`
-do
-    if [ -f $file ]
-    then
-        path=""
-        if [ "${file##*.}" = "js" ]
+if [ ! -d "$output/images/" ]
+then
+    mkdir "$output/images/"
+fi
+echo "copy images/"
+cp -R images/* "$output/images/"
+
+if [ $1 ]
+then
+    cd $lib
+    for file in `ls`
+    do
+        if [ -f $file ]
         then
-            echo "process file-$file"
-            cat $file | eval "sed $reg_script" | eval $tpl_proc | eval $compress_proc > "../$output/$file"
-        else
-            if [ "${file##*.}" = "css" ]
+            path=""
+            if [ "${file##*.}" = "js" ]
             then
                 echo "process file-$file"
-                cat $file | eval $css_proc > "../$output/$file"
+                cat $file | eval "sed $reg_script" | eval $tpl_proc | eval $compress_proc > "$output/$file"
+            else
+                if [ "${file##*.}" = "css" ]
+                then
+                    echo "process file-$file"
+                    cat $file | eval $css_proc > "$output/$file"
+                fi
             fi
         fi
-    fi
-done
-if [ ! -d "../$output/images/" ]
-then
-    mkdir "../$output/images/"
+    done
+    cd ..
+
+    cd $1
+    rm "common"
+
+    cd $output
+    tar -zcvf "../$1.tar.gz" *
+    rm -rf $output
+else
+    cd $output
+    tar -zcvf "../output.tar.gz" *
+    rm -rf $output
 fi
-cp -R images/* "../$output/images/"
+
 cd ..
-
-rm "$1/common"
-
-#cd $output
-#tar -zcvf "../$1.tar.gz" *
-#cd ..
-
-#rm -rf $output
-
 if [ $flag ]
 then
     cd lib-fe
