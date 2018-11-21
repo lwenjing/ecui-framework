6a0d656356e2f64da701e571349063f851f3ae50
linqunyue <linqy@fapiao.com>
2018-11-21 09:38:22 +0800

    样式bug修复

--- common/js/utility.js
+++ common/js/utility.js
@@ -457,7 +457,7 @@ fapiao.TableListRoute.prototype.onbeforerequest = function (context) {
 
 fapiao.TableListRoute.prototype.onbeforerender = function (context) {
     var data = ecui.util.parseValue(this.model[0].split('@')[0], context);
-    var pageNo = data.currentPage || 1;
+    var pageNo = context.currentPage || 1;
     var total = data.count || 0;
     var pageSize = data.pageSize || 10;
     context.page = {
