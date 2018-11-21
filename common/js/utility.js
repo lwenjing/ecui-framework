a037acbbbe5161291dd606afe2fa1dade56e6ebb
liuwenjing <15797707647@163.com>
2018-11-21 17:26:34 +0800

    金额转大写修改

--- common/js/utility.js
+++ common/js/utility.js
@@ -128,32 +128,32 @@ fapiao.util = {
         return ret;
     },
     DX: function (n) {
-        var oldN = n;
-        // 判断正负数
-        if (n.substr(0, 1) == '-') {
-            n = n.substr(1);
-        }
+        var fraction = ['角', '分'];
+        var digit = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖'];
+        var unit = [ ['元', '万', '亿'], ['', '拾', '佰', '仟']  ];
+        var head = n < 0? '负': '';
+        n = Math.abs(n);
 
-        if (!/^(0|[1-9]\d*)(\.\d+)?$/.test(n)) {
-            return '数据非法';
+        var s = '';
+
+        for (var i = 0; i < fraction.length; i++)
+        {
+            s += (digit[Math.floor(n * 10 * Math.pow(10, i)) % 10] + fraction[i]).replace(/零./, '');
         }
-        var unit = '千百拾亿千百拾万千百拾元角分', str = '';
-        n += '00';
-        var p = n.indexOf('.');
-        if (p >= 0) {
-            n = n.substring(0, p) + n.substr(p + 1, 2);
+        s = s || '整';
+        n = Math.floor(n);
+
+        for (var i = 0; i < unit[0].length && n > 0; i++)
+        {
+            var p = '';
+            for (var j = 0; j < unit[1].length && n > 0; j++)
+            {
+                p = digit[n % 10] + unit[1][j] + p;
+                n = Math.floor(n / 10);
+            }
+            s = p.replace(/(零.)*零$/, '').replace(/^$/, '零')  + unit[0][i] + s;
         }
-        unit = unit.substr(unit.length - n.length);
-        for (var i = 0; i < n.length; i++) {
-            str += '零壹贰叁肆伍陆柒捌玖'.charAt(n.charAt(i)) + unit.charAt(i);
-        }
-        str = str.replace(/零(千|百|拾|角|分)/g, '零').replace(/(零)+/g, '零').replace(/零(万|亿|元)/g, '$1').replace(/(亿)万|壹(拾)/g, '$1$2').replace(/^元零?|零分/g, '').replace(/元$/g, '元整');
-        // 判断正负数
-        if (oldN.substr(0, 1) == '-') {
-            n = n.substr(1);
-            str = '负' + str;
-        }
-        return str;
+        return head + s.replace(/(零.)*零元/, '元').replace(/(零.)+/g, '零').replace(/^整$/, '零元整');
     }
 };
 
